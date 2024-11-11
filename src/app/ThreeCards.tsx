72f06446d3a5e3428cee9f1026a428c074132fb7
RajaHadi <shabanajawed411@gmail.com>
2024-11-11 11:20:39 -0800

    @lol

---
+++ src/app/ThreeCards.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Image from "next/image";
+
+const ThreeCards = () => {
+  return (
+    <div className="flex gap-6 mx-9 mt-4">
+    <div className=" w-4/12  ">
+      <Image src={"/new.jpg"} alt="No Image" width={800} height={800} className='h-[288px]'/>
+    </div>
+    <div className=" w-4/12 ">
+      <Image
+        src={"/raja.jpg"}
+        alt="No Image"
+        width={800}
+        height={800}
+        className="h-[288px]"
+      />
+    </div>
+    <div className=" w-4/12 ">
+      <Image
+        src={"/card3.jpg"}
+        alt="No Image"
+        width={800}
+        height={800}
+        className="h-[288px]"
+      />
+    </div>
+  </div>
+  )
+}
+
+export default ThreeCards
