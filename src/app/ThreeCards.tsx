c66b02bb264a1675c052e82e425521d61f98a1cd
RajaHadi <shabanajawed411@gmail.com>
2024-11-17 10:12:18 -0800

    @responsiveness

--- src/app/ThreeCards.tsx
+++ src/app/ThreeCards.tsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 
 const ThreeCards = () => {
   return (
-    <div className="flex gap-6 mx-9 mt-4">
-    <div className=" w-4/12  ">
+    <div className="flex flex-col md:flex-row gap-6 mx-9 mt-4">
+    <div className="w-full md:w-4/12  ">
       <Image src={"/new.jpg"} alt="No Image" width={800} height={800} className='h-[288px]'/>
     </div>
-    <div className=" w-4/12 ">
+    <div className=" w-full md:w-4/12  ">
       <Image
         src={"/raja.jpg"}
         alt="No Image"
@@ -16,7 +16,7 @@ const ThreeCards = () => {
         className="h-[288px]"
       />
     </div>
-    <div className=" w-4/12 ">
+    <div className=" w-full md:w-4/12   ">
       <Image
         src={"/card3.jpg"}
         alt="No Image"
--- src/app/Twocards.tsx
+++ src/app/Twocards.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 
 const Twocards = () => {
   return (
-   <div className="flex gap-4 m-9">  
-    <div className="w-6/12 "><Image src={"/big1.jpg"} alt="Sorry" width={800} height={1000} className="h-96"/></div>
-    <div className="w-6/12  "><Image src={"/big2.jpg"} alt="Sorry" width={800} height={1000} className="h-96"/></div>
+   <div className="flex flex-col md:flex-row gap-4 m-9">  
+    <div className="w-full md:w-6/12 "><Image src={"/big1.jpg"} alt="Sorry" width={800} height={1000} className="h-96"/></div>
+    <div className="w-full md:w-6/12  "><Image src={"/big2.jpg"} alt="Sorry" width={800} height={1000} className="h-96"/></div>
    </div>
   );
 };
